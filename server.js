abb1ae1fd24d2dee5fd48beaa713631045b03315
Magnetic_Soul <everything_90s@yahoo.com>
2024-12-23 20:52:11 -0700

    change code for mongoose

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // const express = require('express');
+// const mongoose = require('mongoose');
 
 import express from 'express';
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
